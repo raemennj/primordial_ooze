a2847ed83fd26b5ee56951d9885f77ea0c4bac62
raemennj <4725458+raemennj@users.noreply.github.com>
2026-05-09 20:06:20 -0700

    Add complete app icon set

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,27 @@
-const CACHE_NAME = 'primordial-ooze-v1';
+const CACHE_NAME = 'primordial-ooze-v2';
 const APP_SHELL = [
   './',
   './index.html',
   './manifest.webmanifest',
   './ooze-header.png',
-  './icon-192.svg',
-  './icon-512.svg'
+  './favicon-16x16.png',
+  './favicon-32x32.png',
+  './apple-touch-icon.png',
+  './icon-16x16.png',
+  './icon-32x32.png',
+  './icon-48x48.png',
+  './icon-72x72.png',
+  './icon-96x96.png',
+  './icon-128x128.png',
+  './icon-144x144.png',
+  './icon-152x152.png',
+  './icon-167x167.png',
+  './icon-180x180.png',
+  './icon-192x192.png',
+  './icon-384x384.png',
+  './icon-512x512.png',
+  './maskable-icon-192x192.png',
+  './maskable-icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
